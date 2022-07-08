1a97592898af161f18d423ca0d6d54a8af7acb82
Vincent Kashaka <vincekashaka@gmail.com>
2022-07-08 19:37:31 +0200

    Added DB config file

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const colors = require('colors');
+const connectDB = require('./config/db');
 const dotenv = require('dotenv').config();
+
+connectDB();
 
 const port = process.env.PORT || 5000;
 
